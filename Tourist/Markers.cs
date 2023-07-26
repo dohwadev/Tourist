28f1c2bb9f77c8116a06020e0dcf9a6b83278bf3
DoHwa <kangdohwa0310@gmail.com>
2023-07-26 19:21:03 +0900

    Initial Commit

---
+++ Tourist/Markers.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Dalamud.Game;
+using Dalamud.Logging;
+using Lumina.Excel.GeneratedSheets;
+
+namespace Tourist {
+    public class Markers : IDisposable {
+        private const string MarkerPath = "bgcommon/world/common/vfx_for_live/eff/b0810_tnsk_y.avfx";
+
+        private Plugin Plugin { get; }
+        private Dictionary<uint, IntPtr> Spawned { get; } = new();
+        private HashSet<IntPtr> Queue { get; } = new();
+
+        public Markers(Plugin plugin) {
+            this.Plugin = plugin;
+
+            this.Plugin.ClientState.TerritoryChanged += this.OnTerritoryChange;
+            this.Plugin.Framework.Update += this.OnFrameworkUpdate;
+
+            if (this.Plugin.Config.ShowArrVistas) {
+                this.SpawnVfxForCurrentZone(this.Plugin.ClientState.TerritoryType);
+            }
+        }
+
+        public void Dispose() {
+            this.Plugin.Framework.Update -= this.OnFrameworkUpdate;
+            this.Plugin.ClientState.TerritoryChanged -= this.OnTerritoryChange;
+            this.RemoveAllVfx();
+        }
+
+        internal void RemoveVfx(ushort index) {
+            var adventure = this.Plugin.DataManager.GetExcelSheet<Adventure>()!
+                .Skip(index)
+                .First();
+
+            if (!this.Spawned.TryGetValue(adventure.RowId, out var vfx)) {
+                return;
+            }
+
+            this.Plugin.Functions.RemoveVfx(vfx);
+            this.Spawned.Remove(adventure.RowId);
+        }
+
+        internal void RemoveAllVfx() {
+            foreach (var vfx in this.Spawned.Values) {
+                this.Plugin.Functions.RemoveVfx(vfx);
+            }
+
+            this.Spawned.Clear();
+        }
+
+        internal void SpawnVfxForCurrentZone(ushort territory) {
+            var row = 0;
+            foreach (var adventure in this.Plugin.DataManager.GetExcelSheet<Adventure>()!) {
+                if (row >= 80) {
+                    break;
+                }
+
+                row += 1;
+
+                if (adventure.Level.Value!.Territory.Row != territory) {
+                    continue;
+                }
+
+                if (this.Plugin.Functions.HasVistaUnlocked((short) (row - 1))) {
+                    continue;
+                }
+
+                var loc = adventure.Level.Value;
+                var pos = new Vector3(loc.X, loc.Z, loc.Y + 0.5f);
+                var vfx = this.Plugin.Functions.SpawnVfx(MarkerPath, pos);
+                this.Spawned[adventure.RowId] = vfx;
+                this.Queue.Add(vfx);
+            }
+        }
+
+        private void OnTerritoryChange(object? sender, ushort territory) {
+            if (!this.Plugin.Config.ShowArrVistas) {
+                return;
+            }
+
+            try {
+                this.RemoveAllVfx();
+                this.SpawnVfxForCurrentZone(territory);
+            } catch (Exception ex) {
+                PluginLog.LogError(ex, "Exception in territory change");
+            }
+        }
+
+        private void OnFrameworkUpdate(Framework framework) {
+            foreach (var vfx in this.Queue.ToArray()) {
+                if (this.Plugin.Functions.PlayVfx(vfx)) {
+                    this.Queue.Remove(vfx);
+                }
+            }
+        }
+    }
+}
---
+++ Tourist/Plugin.cs
@@ -0,0 +1,66 @@
+﻿using Dalamud.Data;
+using Dalamud.Game;
+using Dalamud.Game.ClientState;
+using Dalamud.Game.Command;
+using Dalamud.Game.Gui;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using FFXIVWeather.Lumina;
+
+namespace Tourist {
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class Plugin : IDalamudPlugin {
+        public string Name => "Tourist";
+
+        internal DalamudPluginInterface Interface { get; }
+
+        [PluginService]
+        internal ClientState ClientState { get; init; } = null!;
+
+        [PluginService]
+        internal CommandManager CommandManager { get; init; } = null!;
+
+        [PluginService]
+        internal DataManager DataManager { get; init; } = null!;
+
+        [PluginService]
+        internal Framework Framework { get; init; } = null!;
+
+        [PluginService]
+        internal GameGui GameGui { get; init; } = null!;
+
+        [PluginService]
+        internal SigScanner SigScanner { get; init; } = null!;
+
+        internal Configuration Config { get; }
+        internal PluginUi Ui { get; }
+        internal FFXIVWeatherLuminaService Weather { get; }
+        internal GameFunctions Functions { get; }
+        private Commands Commands { get; }
+        internal Markers Markers { get; }
+
+        public Plugin(DalamudPluginInterface pluginInterface) {
+            this.Interface = pluginInterface;
+
+            this.Config = this.Interface.GetPluginConfig() as Configuration ?? new Configuration();
+            this.Config.Initialise(this);
+
+            this.Weather = new FFXIVWeatherLuminaService(this.DataManager.GameData);
+
+            this.Functions = new GameFunctions(this);
+
+            this.Markers = new Markers(this);
+
+            this.Ui = new PluginUi(this);
+
+            this.Commands = new Commands(this);
+        }
+
+        public void Dispose() {
+            this.Commands.Dispose();
+            this.Ui.Dispose();
+            this.Markers.Dispose();
+            this.Functions.Dispose();
+        }
+    }
+}
